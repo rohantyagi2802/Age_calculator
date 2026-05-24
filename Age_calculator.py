17d12bdcecd55a03465305f3e3236a096e6e1bab
Rohan <rohan2802tyagi@gmail.com>
2026-05-24 10:48:53 +0530

    Some modifications
    Updated age calculation to include months and days.

--- Age_calculator.py
+++ Age_calculator.py
@@ -1,17 +1,23 @@
 from datetime import date
-#function to calculate the age by date of birth 
+from dateutil.relativedelta import relativedelta
+
 def calculate_age(day, month, year):
     today = date.today()
     birthdate = date(year, month, day)
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    return age
 
-if __name__=="__main__":
- try:
-    day = input('Enter day:')
-    month = input('Enter month:')
-    year = input('Enter year:')
-    age_result = calculate_age(int(day), int(month), int(year))
-    print(f'You are {age_result} years old')
- except:
-    print(f'Failed to calculate age, either day or month or year is invalid')
+    diff = relativedelta(today, birthdate)
+
+    return diff.years, diff.months, diff.days
+
+if __name__ == "__main__":
+    try:
+        day = int(input("Enter day: "))
+        month = int(input("Enter month: "))
+        year = int(input("Enter year: "))
+
+        years, months, days = calculate_age(day, month, year)
+
+        print(f"You are {years} years, {months} months, and {days} days old")
+
+    except:
+        print("Invalid date entered")

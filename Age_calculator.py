6722a44e6363977e8b13b1c47a3b4df0301e5eba
Rohan_tyagi <rohan2802tyagi@gmail.com>
2026-05-24 10:24:14 +0530

    Add my python file for age calculator

---
+++ Age_calculator.py
@@ -0,0 +1,17 @@
+from datetime import date
+#function to calculate the age by date of birth 
+def calculate_age(day, month, year):
+    today = date.today()
+    birthdate = date(year, month, day)
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+if __name__=="__main__":
+ try:
+    day = input('Enter day:')
+    month = input('Enter month:')
+    year = input('Enter year:')
+    age_result = calculate_age(int(day), int(month), int(year))
+    print(f'You are {age_result} years old')
+ except:
+    print(f'Failed to calculate age, either day or month or year is invalid')
